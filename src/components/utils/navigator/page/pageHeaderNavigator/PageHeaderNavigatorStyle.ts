6e0b5b32888288281699dc3bbe563e61ae7fdf0c
thiagossouza <thiagossouzarv@gmail.com>
2023-03-28 22:38:46 -0300

    ajuste modal IOS

--- src/components/utils/navigator/page/pageHeaderNavigator/PageHeaderNavigatorStyle.ts
+++ src/components/utils/navigator/page/pageHeaderNavigator/PageHeaderNavigatorStyle.ts
@@ -2,10 +2,11 @@ import styled from "styled-components/native"
 import { ThemeObject } from "../../../../../core/theme"
 import { BaseText } from "../../../../core"
 import { Icon } from "../../../../ui"
+import { Platform } from 'react-native';
 
 export const Container = styled.View`
     flex-direction: row;
-    padding: ${({ theme }: ThemeObject) => `${theme.metrics.spacing.md}px ${theme.metrics.spacing.md * 1.2}px`};
+    padding: ${({ theme }: ThemeObject) => Platform.OS == 'ios'? theme.metrics.spacing.lg + 20: theme.metrics.spacing.md}px 0px;
     padding-bottom: ${({ theme }: ThemeObject) => theme.metrics.spacing.lg * .7}px;
     align-items: center;
     background-color: ${({ theme }: ThemeObject) => theme.header.bg.primary};
